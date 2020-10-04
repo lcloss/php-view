8091c1f4eaae349f9b396e3bdf2060fc6bb19581
Luciano Closs <lcloss@gmail.com>
2020-10-05 00:39:38 +0100

    Initial commit

--- tests/autoload.php
+++ tests/autoload.php
@@ -1,37 +1,29 @@
 <?php
 define('ROOT_FOLDER', __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR);
+define('THIS_NAMESPACE', 'LCloss\\View\\');
 
-spl_autoload_register(function ($class) {
-    // First take a look on 'packages' folder:
-    $package_path = "";
-    $path_to = explode( '\\', $class );
-    $parts = count( $path_to );
-    $vendor = "";
-    $class_name = "";
-
-
-    if ( $parts > 1 ) {
-        $is_first = true;
-        foreach ( $path_to as $i => $path ) {
-            if ( !$is_first ) {
-                $package_path .= DIRECTORY_SEPARATOR;
-            } else {
-                $vendor = $path;
-            }
-            if ( $i <> ( $parts - 1) ) {
-                $package_path .= strtolower( $path );
-            } else {
-                $class_name = $path;
-            }
-            $is_first = false;
+if (!function_exists('startsWith')) {
+    function startsWith( $target, $context ): bool {
+        if ( strlen( $context ) > strlen( $target ) ) {
+            return false;
         }
-
-        $class_path = ROOT_FOLDER . "src" . DIRECTORY_SEPARATOR . $class_name . ".php";
-
-        if ( file_exists( $class_path ) ) {
-            require_once( $class_path );
+        if ( substr( $target, 0, strlen( $context ) ) == $context ) {
+            return true;
+        } else {
+            return false;
         }
     }
+}
+
+spl_autoload_register(function ($class) {
+
+    if ( startsWith( $class, THIS_NAMESPACE )) {
+        $path_to = explode ( '\\', $class );
+        $i = count( $path_to ) - 1;
+        $class_name = $path_to[$i] . '.php';
+        require_once ( ROOT_FOLDER . 'src' . DIRECTORY_SEPARATOR . $class_name);
+    }
+
 });
 
 ?>
